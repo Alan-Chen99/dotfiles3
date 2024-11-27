d128ebf10076ccf72b7f0818e6bcc651a0f291a7
Xinyang Chen <chenxinyang99@gmail.com>
2024-11-26 21:49:57 -0600

    speed up find-library on windows

--- emacs/alan-marginalia.el
+++ emacs/alan-marginalia.el
@@ -8,7 +8,11 @@
   ;; (startup-queue-package 'mule-util 50)
   (startup-queue-package 'marginalia 50))
 
+(defun marginalia--library-doc-ignore (_file)
+  (progn ""))
 (eval-after-load! marginalia
+  (when (eq system-type 'windows-nt)
+    (advice-add #'marginalia--library-doc :override #'marginalia--library-doc-ignore))
   (marginalia-mode))
 
 (provide 'alan-marginalia)
