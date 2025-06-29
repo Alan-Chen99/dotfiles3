ae6e7ec2895bdde640fad0b8eef67ad6ed299f0b
Xinyang Chen <chenxinyang99@gmail.com>
2025-06-28 21:17:04 -0500

    docker python script, some python deps added

--- scripts/rundocker.py
+++ scripts/rundocker.py
@@ -1,20 +1,30 @@
-#!/usr/bin/env -S nix shell p#python.inject.simple-parsing --command python
-
+#!/usr/bin/env -S nix shell p#python.inject.cappa --command python
 import os
 import shlex
 import sys
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from pathlib import Path
+from typing import Annotated
 
-import simple_parsing
+import cappa
+from cappa import Arg
 
 
 @dataclass
 class Args:
-    image: str | None = simple_parsing.field(positional=True, default=None)
+    image: Annotated[str, Arg(short=True)] = "shinsenter/scratch"
+
+    uid: Annotated[int, Arg(short=True)] = 1000
+
+    name: Annotated[str | None, Arg(short=True)] = None
+
+    docker_flags: list[str] = field(default_factory=list)
 
 
-args = simple_parsing.parse(Args)
+args = cappa.parse(Args)
+
+# print(args)
+# assert False
 
 HOME = Path(os.environ["HOME"]).resolve()
 DISPLAY = os.environ["DISPLAY"]
@@ -23,6 +33,8 @@ SSL_CERT_FILE = os.environ.get("SSL_CERT_FILE", "")
 extra_path1 = HOME / ".nix-profile/bin"
 extra_path2 = HOME / ".local/docker_static_bin"
 
+host_bin_path = "/docker_host_bin"
+
 flags: list[str | list[str]] = [
     "docker",
     "run",
@@ -30,31 +42,47 @@ flags: list[str | list[str]] = [
     "--tty",
     "--rm",
     ["--gpus", "all"],
-    ["-v", "/nix:/nix"],
-    ["-v", f"{extra_path1}:/host_bin"],
-    ["-v", f"{extra_path2}:/host_static_bin"],
+    # ["--network", "none"],
+    # "--privileged",
+    # ["--security-opt", "seccomp=unconfined"],
+    # "--cap-add=NET_RAW",
+    ["--name", args.name] if args.name is not None else [],
+    ["-v", f"/nix:/nix"],
+    ["-v", f"{extra_path1}:{host_bin_path}:ro"],
+    #
+    # TODO: this dont work if the base image uses busybox
+    # causing stuff to be symlinked to /bin/sh
+    # which then dont work
+    ["-v", f"{extra_path1}/sh:/bin/sh:ro"],
+    #
+    # ["-v", f"{extra_path1}/bash:/bin/bash:ro"],
+    # ["-v", f"{extra_path1}/ls:/bin/ls:ro"],
+    ["-v", f"{extra_path2}:/host_static_bin:ro"],
     ["-v", f"{HOME}:{HOME}"],
     ["--tmpfs", "/tmp:exec,mode=1777"],
-    ["--tmpfs", "/home/dockeruser:exec,mode=1777"],
-    ["-v", f"{HOME}/.cache/nix:/home/dockeruser/.cache/nix"],
-    ["--user", "1000:0"],
+    ["--tmpfs", f"/home/dockeruser:exec,mode=1777,uid={args.uid},gid=0"],
+    ["-v", f"{HOME}/.cache/nix:/home/dockeruser/.cache/nix"] if args.uid == 0 else [],
+    ["--user", f"{args.uid}:0"],
     ["--workdir", os.getcwd()],
     ["-e", f"DISPLAY={DISPLAY}"],
     ["-e", "HOME=/home/dockeruser"],
     ["-e", f"SSL_CERT_FILE={SSL_CERT_FILE}"],
     ["-e", r"PS1=${debian_chroot:+($debian_chroot)}\u@\H:\w\$ "],
+    ["-e", f"PATH={host_bin_path}:/host_static_bin"],
     ["-v", "/tmp/.X11-unix:/tmp/.X11-unix"],
-    args.image if args.image is not None else "shinsenter/scratch",
-    [
-        "/host_bin/bash",
-        "--norc",
-        "--noprofile",
-        "-i",  # bash otherwise drops $PS1
-        "-c",
-        """
-        exec /host_bin/env PATH="/host_bin:/host_static_bin:${PATH}" bash --norc --noprofile -i
-        """,
-    ],
+    ["--entrypoint", f"{host_bin_path}/bash"],
+    *args.docker_flags,
+    args.image,
+    ["--norc", "--noprofile", "-i"],
+    # [
+    #     "--norc",
+    #     "--noprofile",
+    #     "-i",  # bash otherwise drops $PS1
+    #     "-c",
+    #     """
+    #     exec {host_bin_path}/env PATH="{host_bin_path}:/host_static_bin:${PATH}" bash --norc --noprofile -i
+    #     """,
+    # ],
 ]
 
 
@@ -66,7 +94,7 @@ def gen():
             yield x
 
 
-args = list(gen())
-# print(" ".join(shlex.quote(x) for x in args))
+str_args = list(gen())
+# print(" ".join(shlex.quote(x) for x in str_args))
 
-os.execlp("docker", *args)
+os.execlp("docker", *str_args)
