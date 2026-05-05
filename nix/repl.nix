f47f11ca49789d5d82cd71c0704ef72146ba0317
Xinyang Chen <chenxinyang99@gmail.com>
2026-05-05 18:14:52 -0400

    fix less-download-flakes not working due to `nix-repl`

--- nix/repl.nix
+++ nix/repl.nix
@@ -1,22 +1,37 @@
 {
   self,
   callpackage,
-  std,
+  lib,
   nix,
+  std,
+  nixconf,
+  super,
 }: rec {
-  env = callpackage ./env.nix {} (final: prev: {
-    nixconf =
-      prev.nixconf
-      // {
-        # TODO: nix-doc doesnt build
-        # plugin-files = "${nix-doc}/lib/libnix_doc_plugin.so";
-        allow-unsafe-native-code-during-evaluation = true;
-      };
-  });
+  # does not propagate less-download-flakes, etc correctly
+  # env = callpackage ./env.nix {} (final: prev: {
+  #   nixconf =
+  #     prev.nixconf
+  #     // {
+  #       allow-unsafe-native-code-during-evaluation = true;
+  #     };
+  # });
+
+  nixconf-modified =
+    nixconf
+    // {
+      # TODO: nix-doc doesnt build
+      # plugin-files = "${nix-doc}/lib/libnix_doc_plugin.so";
+      allow-unsafe-native-code-during-evaluation = true;
+    };
+
+  nixconf-file =
+    std.writeText "nixconf"
+    (lib.concatStringsSep "\n" (builtins.attrValues
+        (builtins.mapAttrs (name: value: "${name} = ${builtins.toString value}") nixconf-modified)));
 
   export.nixrepl-wrapper = std.writeScriptBin "nix-repl" ''
     #! ${std.stdenv.shell}
-    export NIX_USER_CONF_FILES=${env.nixconf-file}
+    export NIX_USER_CONF_FILES=${nixconf-file}
     exec ${nix}/bin/nix repl --print-build-logs "$@" --expr "import <prelude>"
     # exec ${nix}/bin/nix repl --print-build-logs "$@"
   '';
